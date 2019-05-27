0f3dc742e198b6e2f23e61daa9c5ac8a0c2890c2
Carlos Augusto <carlos.xell.3@gmail.com>
2019-05-26 22:16:21 -0300

    Type: Refactor
    The subject: criação de botões e rota para página de editar comentários

--- app/src/components/CommentList/CommentList.js
+++ app/src/components/CommentList/CommentList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import './CommentList.css'
 
 class CommentList extends Component {
@@ -8,8 +9,18 @@ class CommentList extends Component {
       <ul className='commentList'>
         { this.props.lista.map((item, ind) => (
           <li className='commentList_item' key={ ind }>
-            <p className='commentList_wrap_author'><strong>Author:</strong> { item.author }</p>
-            <p className='commentList_text'>{ item.body }</p>
+            <div className='commentList_grid'>
+              <div className='commentList_grid_info'>
+                <p className='commentList_wrap_author'><strong>Author:</strong> { item.author }</p>
+                <p className='commentList_text'>{ item.body }</p>
+              </div>
+
+              <div className='commentList_grid_box'>
+                <Link className='commentList_btn' to={`/edit-comment/${ item.id }`}>Edit</Link>
+                <br />
+                <button className='commentList_btn--danger' type='button'>Delete</button>
+              </div>
+            </div>
           </li>
         )) }
       </ul>
---
+++ app/src/views/EditComment.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../assets/css/EditComment.css';
+
+class EditComment extends Component {
+  render() {
+    return(
+      <div className='teste'>Página de edição de comentário</div>
+    )
+  }
+}
+
+export default EditComment;
